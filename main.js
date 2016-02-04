22e57f46d2b97cee6d2ddbfd10a14d44ffe9526a
caleb92 <calebjmcdaniel@gmail.com>
2016-02-04 01:03:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+window.onload(function() {
+  console.log('page loaded');
+  var serializeButton = document.getElementById('serialize');
+  serializeButton.addEventListener('click',function() {
+    console.log('hi there sexy');
+    var inputEl = document.getElementById('input');
+    var outputEl = document.getElementById('output');
+    outputEl.value = JSON.stringify(inputEl.value);
+  });
+})
